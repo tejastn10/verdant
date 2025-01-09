f67e96eae8a30e021fe68676738b0ad83a653244
Tejas Nikhar <tejastn10@gmail.com>
2025-01-09 06:48:38 +0530

    feat: refactor App component to use functional component type and named export

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function App() {
+const App: FC = () => {
 	const [count, setCount] = useState(0);
 
 	return (
@@ -26,6 +26,6 @@ function App() {
 			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
-}
+};
 
-export default App;
+export { App };
