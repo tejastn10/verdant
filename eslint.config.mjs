b712405d6605010c183f5c51387cb77487062996
Tejas Nikhar <tejastn10@gmail.com>
2025-01-09 06:39:54 +0530

    feat: migrate ESLint configuration to new format and remove deprecated files

---
+++ eslint.config.mjs
@@ -0,0 +1,82 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+import tsParser from "@typescript-eslint/parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+});
+
+const eslintConfig = [
+	{
+		ignores: [
+			"**/node_modules",
+			"**/.next",
+			"**/.DS_Store",
+			"**/dist",
+			"**/dist-ssr",
+			"**/*.local",
+			"**/.eslintcache",
+			"**/*.spec.tsx",
+			"**/*.mjs",
+			"**/*.md",
+			"**/*.less",
+			"**/*.css",
+		],
+	},
+	...compat.extends(
+		"@repo/eslint-config/library.js",
+		"plugin:@typescript-eslint/recommended",
+		"prettier"
+	),
+	{
+		languageOptions: {
+			parser: tsParser,
+			ecmaVersion: 2020,
+			sourceType: "module",
+
+			parserOptions: {
+				project: true,
+			},
+		},
+
+		rules: {
+			eqeqeq: "error",
+			"no-unused-vars": "off",
+			"no-implicit-coercion": "error",
+			"consistent-return": "error",
+			semi: "error",
+			quotes: ["error", "double"],
+
+			indent: [
+				"error",
+				"tab",
+				{
+					SwitchCase: 1,
+					VariableDeclarator: 1,
+					ignoredNodes: ["PropertyDefinition[decorators.length > 0]"],
+				},
+			],
+
+			"@typescript-eslint/explicit-function-return-type": "error",
+			"@typescript-eslint/no-implicit-any": "off",
+			"@typescript-eslint/no-unsafe-assignment": "error",
+		},
+	},
+	{
+		files: ["**/*.ts"],
+
+		rules: {
+			"@typescript-eslint/indent": "off",
+		},
+	},
+];
+
+export default eslintConfig;
