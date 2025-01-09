de53688c35d1fd06a1bcb909ed8e8d8bff941585
Tejas Nikhar <tejastn10@gmail.com>
2025-01-09 06:46:48 +0530

    feat: switch server to Fastify adapter and update bootstrap logic with enhanced logging

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
-}
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { Logger } from "@nestjs/common";
+
+const bootstrap = async (): Promise<void> => {
+	const FastifyModule = new FastifyAdapter();
+
+	const PORT = 5000;
+	const MODE = "development";
+
+	const app = await NestFactory.create<NestFastifyApplication>(AppModule, FastifyModule);
+	await app.listen(
+		{
+			port: PORT,
+		},
+		(error, address) => {
+			if (error) {
+				Logger.error(`Failed to start server: ${error.message}`, "Server");
+				return;
+			}
+
+			Logger.verbose(`Server listening on port:${PORT}`, "SERVER");
+			Logger.verbose(`IPv4: http://localhost:${PORT}`, "SERVER");
+			Logger.verbose(`IPv6: ${address}`, "SERVER");
+
+			Logger.debug(`Server running in ${MODE} mode`, "SERVER");
+		}
+	);
+};
+
 bootstrap();
